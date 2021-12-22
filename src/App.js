0836516c64a09c9c6a3add93ddbd850f3d742dbb
deltorro2 <molotsky@gmail.com>
2021-12-22 12:12:00 +0200

    the new feature added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  Hello from volume, HEY!
+	  This is the new feature
         </p>
         <a
           className="App-link"
